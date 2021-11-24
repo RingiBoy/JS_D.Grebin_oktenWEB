8a0d315c52bc688b55a94cc0aeec32cdc4ebd46e
RingiBoy <mitya.info@gmail.com>
2021-11-24 16:47:39 +0200

    fin5

--- les5/main.js
+++ les5/main.js
@@ -56,17 +56,52 @@
 // ull('text111111', 'text222222','text3333333')
 // - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
 
-let ul2=(str,nimber)=>{
-    document.write('<ul>')
-    for (let i = 0; i < nimber; i++) {
-        document.write(`<li>${str}</li>`)
+// let ul2=(str,nimber)=>{
+//     document.write('<ul>')
+//     for (let i = 0; i < nimber; i++) {
+//         document.write(`<li>${str}</li>`)
         
-    }
-    document.write('</ul>')
-}
+//     }
+//     document.write('</ul>')
+// }
 
 
-ul2('text for lesson5',6)
+// ul2('text for lesson5',6)
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
 
+// arr=[343,433,'text',true];
+
+
+// let a=(b) => {
+//     document.write('<ul>')
+  
+//         for (let i = 0; i < arr.length; i++) {
+            
+//             document.write(`<li>${arr[i]}</li>`)
+//             }
+//         document.write('</ul>')
+// }
+
+// console.log(a(arr));
+
+
+
+
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// let objS=[{id:1, name:'Dima', age:36}, {id:2, name:'Olya', age:4}, {id:3, name:'Yura', age:6}, {id:4, name:'Serg', age:17}]
+// let massObj=(params)=>{
+
+//     for (const obj of params) {
+//                 document.write('<div>')
+                
+//                 for (const key in obj) {
+//                     const element = obj[key];
+//                     document.write(`<p>${element}</p>`)
+//                     }
+//                 document.write('</div>')
+//             }
+
+// }
+
+
+// massObj(objS)
