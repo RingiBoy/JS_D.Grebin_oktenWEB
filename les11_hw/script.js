126ab32f0edf6295940a0a30993b8c6d9b61ba5b
RingiBoy <mitya.info@gmail.com>
2021-12-09 23:21:26 +0200

    fin

--- les11_hw/script.js
+++ les11_hw/script.js
@@ -19,17 +19,15 @@
 // -створити форму з інпутами для model,type та volume автівки.
 // при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
 
-// let inp1 = document.getElementsByClassName('input')[0];
-// let inp2 = document.getElementsByClassName('input')[1];
-// let inp3 = document.getElementsByClassName('input')[2];
-// arr=[
-    
-//   ];
-// let frm = document.forms.f2
-// frm.onsubmit = function(e){    
-//     e.preventDefault(); 
-//    arr.push({model:inp1.value, type:inp2.value, volume:inp3.value}); 
+let inp1 = document.getElementsByClassName('input')[0];
+let inp2 = document.getElementsByClassName('input')[1];
+let inp3 = document.getElementsByClassName('input')[2];
+arr=[];
+let frm = document.forms.f2
+frm.onsubmit = function(e){    
+    e.preventDefault(); 
+   arr.push({model:inp1.value, type:inp2.value, volume:inp3.value}); 
   
-// localStorage.setItem('arr', JSON.stringify(arr));
+localStorage.setItem('arr', JSON.stringify(arr));
 
-// }
+}
