f1cc0d7632f146f3ade6d18c1719bfdf86d58db9
RingiBoy <mitya.info@gmail.com>
2021-12-21 23:19:24 +0200

    fin project

--- JS_mini_project/userDetails.js
+++ JS_mini_project/userDetails.js
@@ -8,15 +8,18 @@ let arrLocalStorege=JSON.parse(techStack);
 
 let wrap = document.createElement('div');
 document.body.appendChild(wrap)   //главный блок объекта
-wrap.setAttribute('class', 'wrap')
+wrap.setAttribute('id', 'wrap')
 
     
     
     let blok = document.createElement('div');
     blok.setAttribute('class', 'list-group-item list-group-item-dark')
-   
+    let blokRight = document.createElement('div');
+    blokRight.setAttribute('class', 'list-group-item list-group-item-dark')
     //let start key
     let idAdd = document.createElement('p');
+    let button = document.createElement('button');
+    button.setAttribute('class','btn btn-secondary btn-lg')
     let nameAdd = document.createElement('p');
     let username=document.createElement('p');
     let email = document.createElement('p');
@@ -44,6 +47,7 @@ wrap.setAttribute('class', 'wrap')
 
     //innerText allDate:
     idAdd.innerText = `ID:   ${arrLocalStorege.id}`;
+    button.innerText='go to '
     nameAdd.innerText = `NAME:   ${arrLocalStorege.name}`;
     username.innerText = `USERNAME:   ${arrLocalStorege.username}`;
     email.innerText = `EMAIL:   ${arrLocalStorege.email}`;
@@ -61,13 +65,18 @@ wrap.setAttribute('class', 'wrap')
     nameCompany.innerText=`NAME:  ${arrLocalStorege.company.name}`;
     catchPhrase.innerText=`PHRASE:  ${arrLocalStorege.company.catchPhrase}`;
     bs.innerText=`BS:  ${arrLocalStorege.company.bs}`;
+    button.innerText='post of current user';
+
+
+
+    
 
 
     
     //append wrap:
-    wrap.appendChild(blok);
+    wrap.append(blok, blokRight);
     //append block:
-    blok.appendChild(idAdd);
+    blok.append(button,idAdd);
     blok.appendChild(nameAdd);
     blok.appendChild(username);
     blok.appendChild(email);
@@ -87,8 +96,36 @@ wrap.setAttribute('class', 'wrap')
     geo.appendChild(lng);
     //append company:
     company.append(nameCompany,catchPhrase,bs)
+    // blok.appendChild(button);
 
 // 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
 // (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
-// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
 
+button.onclick = function(){
+    // console.log(arrLocalStorege.id)
+    fetch(`https://jsonplaceholder.typicode.com/users/${arrLocalStorege.id}/posts`)
+    .then(value=>value.json())
+    .then(posts=>{
+        for (let i = 0; i < posts.length; i++) {
+            const post = posts[i];
+            
+let titlePost= document.createElement('p');
+titlePost.innerText=post.title;
+
+
+// 6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
+let btn = document.createElement('button');
+btn.innerText = 'go to post details'
+blokRight.append(titlePost,btn);
+
+btn.onclick= function(){ 
+    // console.log(post.id)
+    document.location='post-details.html'; 
+    localStorage.setItem('post', JSON.stringify(post));
+}
+
+        }
+        
+        
+    });
+}
